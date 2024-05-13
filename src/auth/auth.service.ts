eee2f729d24a7de67fc27e0519c8f06495c61821
Ali Farooq <alifarooq122@gmail.com>
2024-05-13 23:39:39 +0500

    implemented jwt authentication

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersRepository } from './users.repository';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './jwt-payload.interface';
 
 @Injectable()
 export class AuthService {
-  constructor(private userRepository: UsersRepository) {}
+  constructor(
+    private userRepository: UsersRepository,
+    private jwtService: JwtService,
+  ) {}
 
   async signup(authCredentials: AuthCredentialsDto): Promise<void> {
     return this.userRepository.createUser(authCredentials);
+  }
+
+  async signIn(
+    authCredentials: AuthCredentialsDto,
+  ): Promise<{ accessToken: string }> {
+    const { username, password } = authCredentials;
+    const user = await this.userRepository.findOne({ where: { username } });
+
+    if (!(user && (await bcrypt.compare(password, user.password)))) {
+      throw new UnauthorizedException('please check your login credentials');
+    }
+
+    const payload: JwtPayload = { username };
+    const accessToken = await this.jwtService.sign(payload);
+    return { accessToken };
   }
 }
